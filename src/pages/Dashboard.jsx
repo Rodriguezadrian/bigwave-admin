84b7fa710d6c27d3934b51625ccad1f6f4feaecb
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-13 23:18:50 -0300

    update charts for concole errors

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,8 @@
 import * as React from "react";
-import CssBaseline from "@mui/joy/CssBaseline";
 import Box from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Breadcrumbs from "@mui/joy/Breadcrumbs";
-import Typography from "@mui/joy/Typography";
-import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
-import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
-import DownloadRoundedIcon from "@mui/icons-material/DownloadRounded";
-import Sidebar from "../components/Sidebar";
 import OrderTable from "../components/OrderTable";
 import OrderList from "../components/OrderList";
-import Header from "../components/Header";
-import ChartProductsSold from "../components/ChartProductsSold";
-import BarChartUsers from "../components/BarChartUsers";
-import { Link } from "react-router-dom";
+import ChartsProducts from "../components/ChartProducts";
 
 function Dashboard() {
   return (
@@ -37,14 +26,9 @@ function Dashboard() {
           gap: 1,
         }}
       >
-        <div className="chart-father">
-          <div className="chart-container">
-            <ChartProductsSold />
-          </div>
-          <div className="chart-container">
-            <BarChartUsers />
-          </div>
-        </div>
+          <ChartsProducts />
+
+     
         <OrderTable />
         <OrderList />
       </Box>
